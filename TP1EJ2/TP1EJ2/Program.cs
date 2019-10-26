8d7203d8ccccc6b78968b00aeefcecccca8bf723
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-26 12:45:32 -0300

    TP!EJ2 26-10-2019

---
+++ TP1EJ2/TP1EJ2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1EJ2
+{
+    class Cubo
+    {
+        private double lado, area, volumen;
+
+        Cubo()
+        {
+
+        }
+
+        public double Lado
+        {
+            set
+            {
+                lado = value;
+            }
+        }
+        public double Area
+        {
+            set
+            {
+                area = value;
+            }
+        }
+
+        public double Volumen
+        {
+            set
+            {
+                volumen = value;
+            }
+        }
+
+        public double calcularVolumen(double lado)
+        {
+            volumen = lado * lado * lado;
+            return volumen;
+        }
+
+        public double calcularArea(double lado)
+        {
+            area = lado * lado * 6;
+            return area;
+        }
+        static void Main(string[] args)
+        {
+            Cubo miCubo = new Cubo();
+            double v, a;
+            Console.Write("Ingresar medida del lado: ");
+            miCubo.lado = double.Parse(Console.ReadLine());
+            v = miCubo.calcularVolumen(miCubo.lado);
+            a = miCubo.calcularArea(miCubo.lado);
+            Console.WriteLine("Area del cubo: {0}\nVolumen de cubo: {1}", a, v);
+            Console.ReadKey();
+            
+        }
+    }
+}
