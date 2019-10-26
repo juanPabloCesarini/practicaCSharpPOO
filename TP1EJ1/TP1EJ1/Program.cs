5c8823a6bab8e30882966ef2b173571992c6ae32
juanPabloCesarini <jpablocesarini@gmail.com>
2019-10-26 11:49:21 -0300

    TP1EJ1 26-10-2019

---
+++ TP1EJ1/TP1EJ1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace TP1EJ1
+{
+    class Estudiante
+    {
+        private string nombre;
+        private string apellido;
+        private int legajo;
+
+        Estudiante()
+        {
+
+        }
+        Estudiante(string nombre, string apellido, int legajo)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return apellido;
+            }
+        }
+
+        public int Legajo
+        {
+            get
+            {
+                return legajo;
+            }
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine("Su nombre es " + this.Nombre + " y su apellido " + this.Apellido + " y su legajo es: " + this.Legajo);
+        }
+        static void Main(string[] args)
+        {
+            Estudiante Est1 = new Estudiante("Juan", "Gonzalez", 1586);
+            Estudiante Est2 = new Estudiante("Pablo", "Marmol", 8453);
+
+            Est1.imprimir();
+            Est2.imprimir();
+            Console.ReadKey();
+        }
+    }
+}
